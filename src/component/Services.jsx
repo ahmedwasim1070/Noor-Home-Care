2b391ebb00b953bacc30b881eab8092abcd08c41
ahmedwasim1070 <ahmedwasim1070@gmail.com>
2024-11-23 18:06:16 +0500

    finshing up Home page

--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -1,12 +1,4 @@
 import React, { useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
-import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
 function Services() {
   return (
     <section>
