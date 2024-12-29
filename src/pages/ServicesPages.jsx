bb92695396a12c855d1cd0f76b1ef546e295a714
ahmedwasim1070 <ahmedwasim1070@gmail.com>
2024-12-29 21:04:24 +0500

    m

--- src/pages/ServicesPages.jsx
+++ src/pages/ServicesPages.jsx
@@ -70,7 +70,7 @@ function ServicesPages() {
       </div>
       <div
         ref={serviceRef}
-        className="w-full h-full flex items-center 2xl:my-40 xl:my-40 lg:my-40 md:my-40 sm:my-20 esm:my-16 justify-center smothUp opacity-0 transition-opacity duration-1000 ease-in-out"
+        className="w-full h-full flex items-center 2xl:mb-40 xl:mb-40 lg:mb-40 md:mb-40 sm:mb-20 esm:mb-16 justify-center smothUp opacity-0 transition-opacity duration-1000 ease-in-out"
       >
         <div className="2xl:container xl:container lg:container md:container sm:container-none esm:container-none 2xl:mx-auto xl:mx-10 lg:mx-auto md:mx-auto sm:mx-10 esm:mx-2 2xl:p-10 xl:p-16 lg:p-10 md:p-10 sm:p-8 esm:p-4 bg-[#C5FFF8] rounded-2xl flex flex-row items-center justify-center overflow-x-hidden  gap-10 gap-y-20 border-black shadow-2xl shadow-primaryColor flex-wrap">
           <div className="bg-primaryColor rounded-[10px] shadow-primaryColor shadow-2xl flex flex-col 2xl:p-10 xl:p-10 lg:p-10 md:p-10 sm:p-10 esm:p-6 gap-y-10">
